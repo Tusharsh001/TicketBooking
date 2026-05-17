8d48d7f7717a5d114c2315937bde7345487b9751
Tushar Sharma <sharmatushar2505@gmail.com>
2026-05-17 10:54:12 +0530

    basic layout of services

---
+++ seating-service/src/main/java/com/tushar/seating_service/SeatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tushar.seating_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatingServiceApplication.class, args);
+	}
+
+}
